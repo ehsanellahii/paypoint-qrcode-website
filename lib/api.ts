f1b29c6ea392c342344d93aa50a0fbbfd8d316a8
Ehsan Ellahi <118091070+ehsanellahii@users.noreply.github.com>
2026-01-12 18:26:21 +0500

    fix: update API_BASE_URL to use environment variable for production and development

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import { cache } from 'react';
 import { IMenuData, MenuCategory, MenuProduct } from './utils';
 
-export const API_BASE_URL = 'https://api.paypointpos.de/integration';
+export const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.paypointpos.de/integration' : 'http://localhost:4000/integration';
 // export const API_BASE_URL = 'http://localhost:4000/integration';
 const API_HEADERS: {
   'accept': string;
