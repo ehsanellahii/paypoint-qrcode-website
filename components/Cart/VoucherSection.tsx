0ba83cbdc4899e4f57385f6419fc14c789c8a732
Ehsan Ellahi <118091070+ehsanellahii@users.noreply.github.com>
2026-01-20 03:10:18 +0500

    feat: display voucher title alongside code in VoucherSection

--- components/Cart/VoucherSection.tsx
+++ components/Cart/VoucherSection.tsx
@@ -88,7 +88,9 @@ export default function VoucherSection({ storeInfo, disabled }: Props) {
       {isVoucherApplied && appliedVoucher ? (
         <div className='flex items-center justify-between rounded-md bg-gray-50 p-3'>
           <div className='flex flex-col'>
-            {/* <span className='font-semibold'>{appliedVoucher?.code}</span> */}
+            <span className='font-semibold'>
+              {appliedVoucher?.title} - {appliedVoucher?.code}
+            </span>
             <span className='text-sm text-gray-600'>
               {t.discount ?? 'Discount'}: -{formatPrice(discountAmount)}
             </span>
